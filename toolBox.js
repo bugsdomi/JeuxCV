253fbd853384e9383273e5647a3d650d9d85cd8a
BugsDomi <bugsdomi@neuf.fr>
2018-06-04 10:28:58 +0200

    Revue Multi-navigateurs

--- toolBox.js
+++ toolBox.js
@@ -40,9 +40,6 @@
         }
         // --------------------------------------------------------------
         ToolBox.prototype.openCV = function(event){
-        console.log(event.originalTarget.id === 'idCVcourt');
-        console.log(dataBipBip.targetActif === (dataBipBip.maxCompetences-1));
-
             if (event.originalTarget.id === 'idCVLong'){
                 open('./index3.html', 'CV', 'directories=yes,menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes,toolbar=yes');
             } else {
